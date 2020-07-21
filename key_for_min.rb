c0e34c8f599411990480a7538715b3ba33e08ad6
Jay Oliver <jaykamas@gmail.com>
2020-07-21 19:08:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,11 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   min_value = 10000000000
+<<<<<<< HEAD
   correct_key = nil
+=======
+  correct_key = :symbol
+>>>>>>> 2d64c5faf022b837c9693e4bed3f0b189f461470
   
   name_hash.each do |key, value|
     if value < min_value  
@@ -13,8 +17,14 @@ def key_for_min_value(name_hash)
     end
   end 
   name_hash.each do |key, value|
+<<<<<<< HEAD
     if value == min_value 
       correct_key = key 
+=======
+    if key == min_value 
+      correct_key = key 
+      binding.pry
+>>>>>>> 2d64c5faf022b837c9693e4bed3f0b189f461470
     end
   end 
     correct_key
